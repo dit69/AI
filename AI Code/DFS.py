f9a4b5448d41c3e409e21d9cd80a553e2567c04a
Jagdish Patil <ironman2999plus1@gmail.com>
2024-11-06 22:53:36 +0530

    Update DFS.py

--- AI Code/DFS.py
+++ AI Code/DFS.py
@@ -1,34 +1,27 @@
-#DFS
-g={
-    0:[1,2],
-    1:[0,3,4],
-    2:[3,0],
-    3:[4,1,2],
-    4:[1,3]
+def dfs_iterative(graph, start):
+    visited = set()
+    stack = [start]
+    
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            print(node, end=" ")
+            visited.add(node)
+            
+            # Add neighbors in reverse to maintain left-to-right order
+            for neighbor in reversed(graph[node]):
+                if neighbor not in visited:
+                    stack.append(neighbor)
+
+# Example usage
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
 }
-def dfs(g,s):
-    vis[s]=1
-    print(s)
-    for c in g[s]:
-        if not vis[c]:
-            dfs(g,c)
-vis=[0]*5
-dfs(g,0) 
 
-
-#BFS
-def bfs(g,s):
-    q=[s]
-    vis=[s]
-    while q:
-        cur=q.pop(0)
-        print(cur)
-        for c in g[cur]:
-            if c not in vis:
-                q.append(c)
-                vis.append(c)
-
-print("BFS:")
-bfs(g,0)
-
-#o(n+m)
+print("Iterative DFS starting from node 'A':")
+dfs_iterative(graph, 'A')
